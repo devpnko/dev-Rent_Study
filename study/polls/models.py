11238456c8d81349170b14f46121c0f099db008b
devPnko <devpnko@gmail.com>
2021-11-19 05:53:51 +0900

    데이터변동 성공
    appstart->setting_polls.apps.PollsConfig(apps.py)

--- study/polls/models.py
+++ study/polls/models.py
@@ -2,26 +2,17 @@ from django.db import models
 
 # Create your models here.
 
-# class Question(models.Model):
-#     question_text = models.CharField(max_length=200)
-#     pub_date = models.DateTimeField('date published')
-
-#     def __str__(self):
-#         return self.question_text
-
-# class Choice(models.Model):
-#     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-#     choice_text = models.CharField(max_length=200)
-#     votes=models.IntegerField(default=0)
-
-#     def __str__(self):
-#         return self.choice_text
-
-class ImageCollector(models.Model):
-    title = models.CharField(max_length=100)
-    url = models.URLField('url', blank=False, null=False)
-    tags = models.CharField(max_length-100, black=True, null=True)
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
 
     def __str__(self):
-        return self.title
-        
+        return self.question_text
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes=models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
